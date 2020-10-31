2a9ca0fb6308dbb80c9f3be7ae8f8ac4024b229e
Eric <ekiesow@mail.csuchico.edu>
2020-10-31 15:00:44 -0700

    update MyCard component

--- src/components/MyCard.js
+++ src/components/MyCard.js
@@ -1,25 +1,41 @@
 import React from 'react';
 import {Avatar, Button, Card, Title, Paragraph} from 'react-native-paper';
+import {StyleSheet} from 'react-native';
 
-const LeftContent = (props) => <Avatar.Icon {...props} icon="folder" />;
+// const LeftContent = (props) => <Avatar.Icon {...props} icon="folder" />;
 
 const MyCard = (props) => (
-  <Card style={props.style}>
+  <Card style={styles.card}>
+    <Card.Cover
+      source={{uri: 'https://picsum.photos/700'}}
+      style={styles.cardCover}
+    />
     <Card.Title
-      title="Card Title"
-      subtitle="Card Subtitle"
-      left={LeftContent}
+      title={props.drinkName}
+      subtitle={props.breweryName}
+      //   left={LeftContent}
     />
     <Card.Content>
-      <Title>{props.title}</Title>
+      {/* <Title>{props.title}</Title> */}
       <Paragraph>{props.content}</Paragraph>
     </Card.Content>
-    <Card.Cover source={{uri: 'https://picsum.photos/700'}} />
-    <Card.Actions>
-      <Button>Cancel</Button>
-      <Button>Ok</Button>
+    <Card.Actions style={styles.cardActions}>
+      <Button>Let's Do It</Button>
     </Card.Actions>
   </Card>
 );
+
+const styles = StyleSheet.create({
+  card: {
+    paddingTop: 50,
+    paddingBottom: 30,
+  },
+  cardActions: {
+    justifyContent: 'center',
+  },
+  cardCover: {
+    resizeMode: 'cover',
+  },
+});
 
 export default MyCard;
