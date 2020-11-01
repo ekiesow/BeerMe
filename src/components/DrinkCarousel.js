6ef08340c1a31b03a707d74d305f83f49f70c60d
Eric <ekiesow@mail.csuchico.edu>
2020-10-31 20:25:19 -0700

    added pagination to the carousel

--- src/components/DrinkCarousel.js
+++ src/components/DrinkCarousel.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Dimensions, StyleSheet} from 'react-native';
-import Carousel from 'react-native-snap-carousel';
+import Carousel, {Pagination} from 'react-native-snap-carousel';
 import MyCard from './MyCard';
 
 const {width: screenWidth} = Dimensions.get('window');
@@ -8,24 +8,49 @@ const ITEM_WIDTH = screenWidth - 60;
 const ITEM_HEIGHT = screenWidth - 60;
 
 const DrinkCarousel = (props) => {
+  const [activeSlide, setActiveSlide] = useState(0);
+
   const renderItem = ({item, index}) => {
     return (
       <View key={index} style={styles.container}>
         <MyCard data={item} style={styles.card} />
+        <Pagination
+          dotsLength={props.data.length}
+          activeDotIndex={activeSlide}
+          dotStyle={{
+            width: 10,
+            height: 10,
+            borderRadius: 5,
+            marginHorizontal: 8,
+            backgroundColor: 'rgba(0, 0, 0, 0.60)',
+          }}
+          inactiveDotStyle={{
+            width: 10,
+            height: 10,
+            borderRadius: 5,
+            marginHorizontal: 8,
+            backgroundColor: 'rgba(0, 0, 0, 0.20)',
+          }}
+          inactiveDotOpacity={0.4}
+          inactiveDotScale={0.8}
+        />
       </View>
     );
   };
 
   return (
-    <Carousel
-      sliderWidth={screenWidth}
-      sliderHeight={screenWidth}
-      itemWidth={ITEM_WIDTH}
-      itemHeight={ITEM_HEIGHT}
-      data={props.data}
-      renderItem={renderItem}
-      style={styles.carousel}
-    />
+    <>
+      <Carousel
+        sliderWidth={screenWidth}
+        sliderHeight={screenWidth}
+        itemWidth={ITEM_WIDTH}
+        itemHeight={ITEM_HEIGHT}
+        data={props.data}
+        renderItem={renderItem}
+        style={styles.carousel}
+        onSnapToItem={(index) => setActiveSlide(index)}
+      />
+    </>
   );
 };
 
