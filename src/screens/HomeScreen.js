ebf0d0b74b391e311dee9a772745b24fbac81a0b
Eric <ekiesow@mail.csuchico.edu>
2020-11-05 12:47:29 -0800

    removed unused import

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {View, StyleSheet} from 'react-native';
 import DrinkCarousel from '../components/DrinkCarousel';
 // import SplashScreen from 'react-native-splash-screen' // TODO look into splash screen when loading drinks from graphql see example: https://github.com/sarthakpranesh/Covid19/blob/master/src/screens/HomeScreen.tsx
 
