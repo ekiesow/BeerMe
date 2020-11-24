b22f092cb3bb5b829f3ba082bc0ca75124af1c55
Eric <ekiesow@mail.csuchico.edu>
2020-11-23 21:48:13 -0800

    Changed the tabs depending on Admin user or not

--- src/components/navigation/TabNavigator.js
+++ src/components/navigation/TabNavigator.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import {Auth} from 'aws-amplify';
+import _ from 'lodash';
 
 import {
+  AdminStackNavigator,
   HomeStackNavigator,
   OrdersStackNavigator,
   ProfileStackNavigator,
@@ -11,6 +14,26 @@ import {
 const Tab = createMaterialBottomTabNavigator();
 
 const BottomTabNavigator = () => {
+  const [admin, setAdmin] = useState(false);
+
+  useEffect(() => {
+    is_admin();
+  }, []);
+
+  async function is_admin() {
+    try {
+      const user = await Auth.currentAuthenticatedUser();
+      const groups =
+        user.signInUserSession.accessToken.payload['cognito:groups'];
+      console.log('groups: ', groups);
+      // using lodash to short circuit to make sure groups is not undefined
+      if (!_.isEmpty(groups) && groups.includes('Admin')) {
+        setAdmin(true);
+      }
+    } catch (err) {
+      console.log('Error getting user: ', err);
+    }
+  }
   return (
     <Tab.Navigator initialRouteName="Home">
       <Tab.Screen
@@ -23,34 +46,53 @@ const BottomTabNavigator = () => {
           ),
         }}
       />
-      <Tab.Screen
-        name="Orders"
-        component={OrdersStackNavigator}
-        options={{
-          tabBarLabel: 'Orders',
-          tabBarIcon: ({color}) => (
-            <MaterialCommunityIcons
-              name="package-variant-closed"
-              color={color}
-              size={26}
-            />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Profile"
-        component={ProfileStackNavigator}
-        options={{
-          tabBarLabel: 'Profile',
-          tabBarIcon: ({color}) => (
-            <MaterialCommunityIcons
-              name="account-circle"
-              color={color}
-              size={26}
-            />
-          ),
-        }}
-      />
+      {admin ? (
+        <Tab.Screen
+          name="Admin"
+          component={AdminStackNavigator}
+          options={{
+            tabBarLabel: 'Admin',
+            tabBarIcon: ({color}) => (
+              <MaterialCommunityIcons
+                name="circle-edit-outline"
+                color={color}
+                size={26}
+              />
+            ),
+          }}
+        />
+      ) : (
+        <>
+          <Tab.Screen
+            name="Orders"
+            component={OrdersStackNavigator}
+            options={{
+              tabBarLabel: 'Orders',
+              tabBarIcon: ({color}) => (
+                <MaterialCommunityIcons
+                  name="package-variant-closed"
+                  color={color}
+                  size={26}
+                />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Profile"
+            component={ProfileStackNavigator}
+            options={{
+              tabBarLabel: 'Profile',
+              tabBarIcon: ({color}) => (
+                <MaterialCommunityIcons
+                  name="account-circle"
+                  color={color}
+                  size={26}
+                />
+              ),
+            }}
+          />
+        </>
+      )}
     </Tab.Navigator>
   );
 };
