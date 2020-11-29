5a05735d3777e6db3cab09b0384e18427f206d26
Eric <ekiesow@mail.csuchico.edu>
2020-11-29 13:34:49 -0800

    refactored code, added associating endpoint with user

--- App.js
+++ App.js
@@ -1,32 +1,68 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {StatusBar} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {withAuthenticator} from 'aws-amplify-react-native';
 import PushNotification from '@aws-amplify/pushnotification';
+import {Auth, Analytics} from 'aws-amplify';
 // import { PushNotificationIOS } from '@react-native-community/push-notification-ios';
 
 import DrawerNavigator from './src/components/navigation/DrawerNavigator';
 
+// get the notification data when notification is received
+PushNotification.onNotification((notification) => {
+  // Note that the notification object structure is different from Android and IOS
+  console.log('in app notification', notification);
+
+  // required on iOS only (see fetchCompletionHandler docs: https://github.com/react-native-community/push-notification-ios#finish)
+  // notification.finish(PushNotificationIOS.FetchResult.NoData);
+});
+
+// get the registration token
+// This will only be triggered when the token is generated or updated.
+PushNotification.onRegister((token) => {
+  console.log('in app registration', token);
+});
+
+// get the notification data when notification is opened
+PushNotification.onNotificationOpened((notification) => {
+  console.log('the notification is opened', notification);
+});
+
+async function associateEndpointWithUser(setUserId) {
+  // retrieve and print the unique internal userid
+  const {
+    attributes: {sub},
+  } = await Auth.currentUserInfo();
+  console.log('userId', sub);
+  setUserId(sub);
+
+  // associate the device endpoint with the user
+  Analytics.updateEndpoint({
+    userId: sub,
+  })
+    .then((data) => {
+      console.log('End Update Endpoint userId', data);
+    })
+    .catch((error) => {
+      console.log('Update Endpoint userId', error);
+    });
+}
+
 const App = () => {
-  // get the notification data when notification is received
-  PushNotification.onNotification((notification) => {
-    // Note that the notification object structure is different from Android and IOS
-    console.log('in app notification', notification);
+  // retrieve and print the endpoint id, for testing only
+  const [endpointId, setEndpointId] = useState('');
+  useEffect(() => {
+    const myEndpointId = Analytics.getPluggable('AWSPinpoint')._config
+      .endpointId;
+    console.log('endpointId', myEndpointId);
+    setEndpointId(myEndpointId);
+  }, []);
 
-    // required on iOS only (see fetchCompletionHandler docs: https://github.com/react-native-community/push-notification-ios#finish)
-    // notification.finish(PushNotificationIOS.FetchResult.NoData);
-  });
-
-  // get the registration token
-  // This will only be triggered when the token is generated or updated.
-  PushNotification.onRegister((token) => {
-    console.log('in app registration', token);
-  });
-
-  // get the notification data when notification is opened
-  PushNotification.onNotificationOpened((notification) => {
-    console.log('the notification is opened', notification);
-  });
+  // associate the device endpoint with the user
+  const [userId, setUserId] = useState('');
+  useEffect(() => {
+    associateEndpointWithUser(setUserId);
+  }, []);
 
   return (
     <>
