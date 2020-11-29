20cce0f094a06a181570e4f4f61fb68e69c848a4
eric <ekiesow@mail.csuchico.edu>
2020-11-29 09:32:12 -0800

    refactored imports and removed starter code comments

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
 import {StatusBar} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
+import {withAuthenticator} from 'aws-amplify-react-native';
 
 import DrawerNavigator from './src/components/navigation/DrawerNavigator';
 
-import {withAuthenticator} from 'aws-amplify-react-native';
-
-const App: () => React$Node = () => {
+const App = () => {
   return (
     <>
       <NavigationContainer>
