0a03b78b64af295b452c26c638efedb143358ca8
Eric <ekiesow@mail.csuchico.edu>
2020-12-05 15:47:00 -0800

    display orders if any exist, starting functionality to create order/orderItem

--- src/screens/OrdersScreen.js
+++ src/screens/OrdersScreen.js
@@ -1,10 +1,52 @@
-import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View, StyleSheet} from 'react-native';
+import {Text} from 'react-native-paper';
+import {API} from 'aws-amplify';
+import {listOrders} from '../../graphql/queries';
+
+const initialState = [
+  {
+    id: '',
+    customerId: '',
+    userId: '',
+    customerEmail: '',
+    createdAt: '',
+    amount: '',
+    status: '',
+    customer: '',
+    shippingAddress: '',
+    orderItems: [],
+  },
+];
 
 const OrdersScreen = () => {
+  const [orders, setOrders] = useState(initialState);
+  useEffect(() => {
+    fetchOrders();
+  }, []);
+
+  async function fetchOrders() {
+    try {
+      const orderData = await API.graphql({
+        query: listOrders,
+        authMode: 'AMAZON_COGNITO_USER_POOLS',
+      });
+      const orderList = orderData.data.listOrders.items;
+      if (orderList.length) setOrders(orderList);
+    } catch (err) {
+      console.log('error fetching orders', err);
+    }
+  }
+
   return (
     <View style={styles.center}>
-      <Text>Orders!</Text>
+      {orders[0].id == '' ? (
+        <>
+          <Text>You have no orders yet! :(</Text>
+        </>
+      ) : (
+        <Text> Orders:</Text>
+      )}
     </View>
   );
 };
